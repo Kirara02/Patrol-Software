8012a52ae2353f90c905a5590984b2abc5c97fba
Kirara Bernstein <fahtul.singaparna@gmail.com>
2025-06-18 16:08:35 +0700

    finish Desktop_AU

--- Desktop_AU/Wm5000lDemo/Form1.Designer.cs
+++ Desktop_AU/Wm5000lDemo/Form1.Designer.cs
@@ -38,13 +38,14 @@
             this.button18 = new System.Windows.Forms.Button();
             this.textse = new System.Windows.Forms.TextBox();
             this.textge = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Location = new System.Drawing.Point(12, 13);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.Size = new System.Drawing.Size(75, 25);
             this.btnOpen.TabIndex = 0;
             this.btnOpen.Text = "Open Usb";
             this.btnOpen.UseVisualStyleBackColor = true;
@@ -52,20 +53,20 @@
             // 
             // textBox1
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 135);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 146);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(389, 229);
+            this.textBox1.Size = new System.Drawing.Size(378, 248);
             this.textBox1.TabIndex = 9;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(13, 66);
+            this.button9.Location = new System.Drawing.Point(13, 72);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(117, 23);
+            this.button9.Size = new System.Drawing.Size(117, 25);
             this.button9.TabIndex = 10;
             this.button9.Text = "Get records";
             this.button9.UseVisualStyleBackColor = true;
@@ -73,9 +74,9 @@
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(135, 66);
+            this.button10.Location = new System.Drawing.Point(145, 72);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(116, 23);
+            this.button10.Size = new System.Drawing.Size(116, 25);
             this.button10.TabIndex = 11;
             this.button10.Text = "Erasure Records";
             this.button10.UseVisualStyleBackColor = true;
@@ -83,9 +84,9 @@
             // 
             // button13
             // 
-            this.button13.Location = new System.Drawing.Point(13, 105);
+            this.button13.Location = new System.Drawing.Point(13, 114);
             this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.Size = new System.Drawing.Size(75, 25);
             this.button13.TabIndex = 13;
             this.button13.Text = "Close Usb";
             this.button13.UseVisualStyleBackColor = true;
@@ -93,9 +94,9 @@
             // 
             // button14
             // 
-            this.button14.Location = new System.Drawing.Point(261, 66);
+            this.button14.Location = new System.Drawing.Point(277, 72);
             this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(113, 23);
+            this.button14.Size = new System.Drawing.Size(113, 25);
             this.button14.TabIndex = 14;
             this.button14.Text = "Adjust Time";
             this.button14.UseVisualStyleBackColor = true;
@@ -103,9 +104,9 @@
             // 
             // button17
             // 
-            this.button17.Location = new System.Drawing.Point(13, 37);
+            this.button17.Location = new System.Drawing.Point(13, 40);
             this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(116, 23);
+            this.button17.Size = new System.Drawing.Size(116, 25);
             this.button17.TabIndex = 20;
             this.button17.Text = "Set Equipment ID";
             this.button17.UseVisualStyleBackColor = true;
@@ -113,9 +114,9 @@
             // 
             // button18
             // 
-            this.button18.Location = new System.Drawing.Point(194, 36);
+            this.button18.Location = new System.Drawing.Point(215, 40);
             this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(110, 23);
+            this.button18.Size = new System.Drawing.Size(110, 25);
             this.button18.TabIndex = 21;
             this.button18.Text = "Get Equipment ID";
             this.button18.UseVisualStyleBackColor = true;
@@ -123,24 +124,35 @@
             // 
             // textse
             // 
-            this.textse.Location = new System.Drawing.Point(135, 37);
+            this.textse.Location = new System.Drawing.Point(135, 40);
             this.textse.Name = "textse";
-            this.textse.Size = new System.Drawing.Size(53, 21);
+            this.textse.Size = new System.Drawing.Size(53, 20);
             this.textse.TabIndex = 22;
             this.textse.Text = "1234";
             // 
             // textge
             // 
-            this.textge.Location = new System.Drawing.Point(311, 37);
+            this.textge.Location = new System.Drawing.Point(331, 40);
             this.textge.Name = "textge";
-            this.textge.Size = new System.Drawing.Size(59, 21);
+            this.textge.Size = new System.Drawing.Size(59, 20);
             this.textge.TabIndex = 23;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(315, 115);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 24;
+            this.btnSave.Text = "Save Data";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 376);
+            this.ClientSize = new System.Drawing.Size(402, 407);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.textge);
             this.Controls.Add(this.textse);
             this.Controls.Add(this.button18);
@@ -172,6 +184,7 @@
         private System.Windows.Forms.Button button18;
         private System.Windows.Forms.TextBox textse;
         private System.Windows.Forms.TextBox textge;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- Desktop_AU/Wm5000lDemo/Form1.cs
+++ Desktop_AU/Wm5000lDemo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -152,5 +153,80 @@ namespace Wm5000AEDemo
             else
                 MessageBox.Show("Please open usb");
         }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (opened >= 0)
+            {
+                string rawData = wmport.GetRecords();
+                string[] lines = rawData.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string logPath = @"C:\Patrol_Log\Logs";
+
+                if (!System.IO.Directory.Exists(logPath))
+                {
+                    System.IO.Directory.CreateDirectory(logPath);
+                }
+                else
+                {
+                    string[] existingFiles = System.IO.Directory.GetFiles(logPath);
+                    foreach (string file in existingFiles)
+                    {
+                        try
+                        {
+                            System.IO.File.Delete(file);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Failed to delete existing file:\n{file}\nError: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+
+                string filePath = System.IO.Path.Combine(logPath, $"patrol_log_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.txt");
+
+                try
+                {
+                    using (System.IO.StreamWriter writer = new System.IO.StreamWriter(filePath, true))
+                    {
+                        foreach (var line in lines)
+                        {
+                            string[] parts = line.Split(',');
+                            if (parts.Length == 3)
+                            {
+                                string cardId = parts[0];
+
+                                string tagRaw = parts[1];
+                                string trimmed = tagRaw.TrimStart('0');
+                                string tag = trimmed.Length > 10
+                                    ? trimmed.Substring(trimmed.Length - 10)
+                                    : trimmed.PadLeft(10, '0');
+
+                                string datetime = parts[2];
+                                string date = datetime.Substring(0, 4) + "/" + datetime.Substring(4, 2) + "/" + datetime.Substring(6, 2);
+                                string time = datetime.Substring(8, 2) + ":" + datetime.Substring(10, 2) + ":" + datetime.Substring(12, 2);
+
+                                string logLine = $"1,1,{tag},{date},{time},{cardId}";
+                                writer.WriteLine(logLine);
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Formatted records saved to file:\n" + filePath, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    textBox1.Text = "Data saved";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error writing file:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please open usb");
+            }
+        }
+
+
+
     }
 }
